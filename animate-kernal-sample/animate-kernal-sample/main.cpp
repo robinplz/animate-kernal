1c21c870890ea98b2532003bbd775f1df64d255a
Robin <robinplz@msn.com>
2018-05-09 14:11:09 +0800

    create the sample project

---
+++ animate-kernal-sample/animate-kernal-sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  animate-kernal-sample
+//
+//  Created by Robin on 2018/5/9.
+//  Copyright © 2018 Robin & Viviun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ animate-kernal-sample/animate-kernal/ak-timing-functions.h
@@ -0,0 +1,19 @@
+//
+//  ak-timing-functions.h
+//  animate-kernal-sample
+//
+//  Created by Robin on 2018/5/9.
+//  Copyright © 2018 Robin & Viviun. All rights reserved.
+//
+
+#ifndef ak_timing_functions_h
+#define ak_timing_functions_h
+
+namespace ak {
+    class TimingFunctions {
+        
+    };
+}
+
+
+#endif /* ak_timing_functions_h */
